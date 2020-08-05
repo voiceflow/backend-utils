e326ce0f1680558a22b660c281bcd252fb5c7b51
Frank Gu <frank@voiceflow.com>
2020-08-05 15:48:51 -0400

    fix: logger ts/js

--- logger.js
+++ logger.js
@@ -5,6 +5,7 @@ const Logger = require('@voiceflow/logger');
 // eslint-disable-next-line no-process-env
 const options = ['local', 'test'].includes(process.env.NODE_ENV) ? { level: 'info', stackTrace: true, pretty: true } : {};
 
-const log = new Logger(options);
+// eslint-disable-next-line new-cap
+const log = new Logger.default(options);
 
 module.exports = log;
