8c560562902ef279c67b866ad77c75c74c9266bb
Xavier Portilla Edo <xavi_tb@hotmail.com>
2021-01-26 19:41:38 +0100

    ci(circleci): added sonar and git hooks (CORE-4768) (#16)
    * ci(circleci): added sonar and git hooks
    
    * fix: move pr check to circleci and eslint finished

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,5 @@
+'use strict';
+
 module.exports = {
   extends: '@voiceflow/eslint-config',
-  rules: {
-    'no-continue': 'off',
-    'quotes': ['error', 'single', 'avoid-escape'],
-    'sonarjs/cognitive-complexity': 'warn',
-    'promise/always-return': 'off',
-  }
 };
