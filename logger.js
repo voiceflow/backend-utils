98cf75e5c677a124cae29f7714b1861875f5c719
leartgjoni-voiceflow <leart@voiceflow.com>
2021-03-01 10:38:00 -0500

    fix: update logger (CORE-5217)

--- logger.js
+++ logger.js
@@ -3,7 +3,7 @@
 const Logger = require('@voiceflow/logger');
 
 // eslint-disable-next-line no-process-env
-const options = ['local', 'test'].includes(process.env.NODE_ENV) ? { level: 'info', stackTrace: true, pretty: true } : {};
+const options = ['local', 'test'].includes(process.env.NODE_ENV) ? { level: 'info', pretty: true } : {};
 
 // eslint-disable-next-line new-cap
 const log = new Logger.default(options);
