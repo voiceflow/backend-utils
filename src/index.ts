d3f744b76d7a840b49969d1a94e0a62c42dda07d
Ben Teichman <ben@voiceflow.com>
2021-05-24 21:04:37 -0400

    fix: export types

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,5 @@ export { default as FixtureGenerator } from './fixtureGenerator';
 export * from './middlewares';
 export { default as ResponseBuilder } from './responseBuilder';
 export * from './services';
+export * from './types';
 export * as Validator from 'express-validator';
